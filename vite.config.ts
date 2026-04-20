47da7ade84c29644846ae8f7ab1200ff83fba925
belkovaart <afania41@gmail.com>
2026-04-20 15:47:05 +0400

    fix: set correct github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? "/task-planner/" : "/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/task-planner/" : "/",
   plugins: [react()],
-});
+}));
